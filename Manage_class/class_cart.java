ca3e22782a1d2df190c6d8cc14def8f537e916a1
mickeymacho6 <80432900+mickeymacho6@users.noreply.github.com>
2021-04-29 21:13:23 -0700

    Add files via upload
    Third commit (forgot to push and commit last time)

--- Manage_class/class_cart.java
+++ Manage_class/class_cart.java
@@ -1,111 +1,110 @@
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.swt.custom.ScrolledComposite;
-
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class class_cart {
+	private static JTextField amount_field;
 
-	protected Shell shell;
-	private Table table;
-	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
-
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		try {
-			class_cart window = new class_cart();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+		// TODO Auto-generated method stub
 
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		Display display = Display.getDefault();
-		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
+		
+		String[] columns = {"Course", "Schedule", "Professor", "Units"};
+		Object[][] data = {
+				{"MATH 102", "MONDAY", "KATHY ZHONG", "3"},
+				{"CSC 131", "TUEDAY", "JAGAN CHIDELLA", "3"},
+				{"CHEM 11A", "WEDNESDAY", "JOSH MOSS", "4"},
+				{"CSC 130", "THURSDAY", "DEVIN COOK", "3"}
+				};
+		
+		JTable class_cart_table = new JTable(data, columns);
+		
+		DefaultTableModel model = new DefaultTableModel();
+		
+		JFrame class_cart_frame = new JFrame("Class Cart");
+		class_cart_frame.getContentPane().setBackground(Color.WHITE);
+		class_cart_frame.getContentPane().setForeground(Color.WHITE);
+		class_cart_frame.setBounds(100, 100, 962, 710);
+		class_cart_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		class_cart_frame.getContentPane().setLayout(null);
+		class_cart_frame.setLocationRelativeTo(null);
+		
+		
+		class_cart_table.setBackground(Color.white);
+		class_cart_table.setForeground(Color.black);
+		class_cart_table.setGridColor(Color.gray);
+		class_cart_table.setSelectionForeground(Color.white);
+		class_cart_table.setRowHeight(30);
+		class_cart_table.setAutoCreateRowSorter(true);
+		
+		JScrollPane pane = new JScrollPane(class_cart_table);
+		pane.setForeground(Color.RED);
+		pane.setBackground(Color.WHITE);
+		pane.setBounds(128, 27, 667, 298);
+		class_cart_frame.getContentPane().add(pane);
+		
+		JLabel units_label = new JLabel("Total units: 13");
+		units_label.setBounds(669, 336, 126, 14);
+		class_cart_frame.getContentPane().add(units_label);
+		
+		JLabel total_classes_label = new JLabel("Total classes: 4");
+		total_classes_label.setBounds(498, 336, 109, 14);
+		class_cart_frame.getContentPane().add(total_classes_label);
+		
+		JButton pay_button = new JButton("Pay fees");
+		pay_button.setBounds(388, 543, 89, 23);
+		class_cart_frame.getContentPane().add(pay_button);
+		
+		
+		pay_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+				if (amount_field.getText() == "3600" ) {
+						
+					JOptionPane.showMessageDialog(class_cart_frame, "Fees paid successfully. Thank you!");
+				}
+				
+				else JOptionPane.showMessageDialog(class_cart_frame, "Payment error!");
 			}
-		}
-	}
-
-	/**
-	 * Create contents of the window.
-	 */
-	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(666, 783);
-		shell.setText("Class Cart");
-		shell.setLayout(null);
+		});
 		
-		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setBounds(118, 31, 427, 193);
-		table.setToolTipText("");
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
 		
-		TableColumn course = new TableColumn(table, SWT.CENTER);
-		course.setWidth(124);
-		course.setText("Course");
 		
-		TableColumn schedule = new TableColumn(table, SWT.CENTER);
-		schedule.setWidth(100);
-		schedule.setText("Schedule");
+		JLabel amount_label = new JLabel("Total amount: $3600");
+		amount_label.setBounds(365, 389, 165, 14);
+		class_cart_frame.getContentPane().add(amount_label);
 		
-		TableColumn professor = new TableColumn(table, SWT.CENTER);
-		professor.setWidth(100);
-		professor.setText("Professor");
+		JLabel enter_amount_label = new JLabel("Enter amount to be paid:");
+		enter_amount_label.setBounds(232, 443, 157, 14);
+		class_cart_frame.getContentPane().add(enter_amount_label);
 		
-		TableColumn units = new TableColumn(table, SWT.CENTER);
-		units.setWidth(100);
-		units.setText("Units");
+		amount_field = new JTextField();
+		amount_field.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				
+				
+				amount_field.getText();
+				
+			}
+		});
+		amount_field.setBounds(425, 440, 182, 20);
+		class_cart_frame.getContentPane().add(amount_field);
+		amount_field.setColumns(10);
 		
-		Label lblPhysa = new Label(shell, SWT.NONE);
-		lblPhysa.setBounds(151, 99, 55, 15);
-		lblPhysa.setText("PHYS 11A");
+		Object[] row = new Object[4];
 		
-		Label lblMath = new Label(shell, SWT.NONE);
-		lblMath.setBounds(151, 78, 55, 15);
-		lblMath.setText("MATH 100");
 		
-		Label lblCsc = new Label(shell, SWT.NONE);
-		lblCsc.setBounds(151, 117, 55, 15);
-		lblCsc.setText("CSC 131");
+		class_cart_frame.revalidate();
+		class_cart_frame.setVisible(true);
 		
-		Label lblAccy = new Label(shell, SWT.NONE);
-		lblAccy.setBounds(151, 138, 55, 15);
-		lblAccy.setText("ACCY 1");
-		
-		Label lblPhil = new Label(shell, SWT.NONE);
-		lblPhil.setBounds(151, 159, 55, 15);
-		lblPhil.setText("PHIL 300");
-		
-		Label lblMwPm = new Label(shell, SWT.NONE);
-		lblMwPm.setBounds(240, 99, 104, 15);
-		lblMwPm.setText("M/W 4:00-5:15 P.M.");
-		
-		Button btnPayFees = new Button(shell, SWT.NONE);
-		btnPayFees.setBounds(179, 295, 75, 25);
-		btnPayFees.setText("Pay fees");
-		
-		Button btnReturnToHome = new Button(shell, SWT.NONE);
-		btnReturnToHome.setBounds(338, 295, 139, 25);
-		btnReturnToHome.setText("Return to home page");
-
 	}
 }
